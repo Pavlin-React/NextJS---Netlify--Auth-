ff511d8ef6bff59c0af47797d92c4f31edfaeee1
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-05-06 09:23:56 +0300

    netlify functions

---
+++ functions/supermario.js
@@ -0,0 +1,12 @@
+exports.handler = () => {
+  console.log( 'function run' );
+
+  let data = { name: 'mario', age: 35, job: 'plumber' }
+
+  // return response to browser
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify( data )
+  }
+}
