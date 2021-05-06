d1b44f7e982f6aebdcf3f3ce65f1f0e978b00c63
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-05-06 16:29:09 +0300

    Client Side Content Guard

--- pages/guides.js
+++ pages/guides.js
@@ -1,32 +1,62 @@
-import { useContext, useEffect, useState } from 'react'
-import AuthContext from '../stores/authContext'
-import styles from '../styles/Guides.module.css'
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../stores/authContext";
+import styles from "../styles/Guides.module.css";
 
 export default function Guides() {
+  let { user, authReady } = useContext(AuthContext);
+  let [guides, setGuides] = useState(null);
+  let [error, setError] = useState(null);
 
-  let { user, authReady } = useContext( AuthContext )
-  let [guides, setGuides] = useState( null )
-  let [error, setError] = useState( null )
-  
-
-  useEffect( () => {
-    if ( authReady ) {
-      fetch( '/.netlify/functions/guides', user && {
-        headers: {
-          Authorization: 'Bearer ' + user.token.access_token
+  useEffect(() => {
+    if (authReady) {
+      fetch(
+        "/.netlify/functions/guides",
+        user && {
+          headers: {
+            Authorization: "Bearer " + user.token.access_token
+          },
         }
-      } )
-        .then( res => {
-          console.log( res );
-        } )
-        .then( data => console.log( data ) )
+      )
+        .then(res => {
+          if (!res.ok) {
+            throw Error("You must be logIn to see this content");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setGuides(data);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setGuides(null);
+        });
     }
-    
-  }, [user, authReady] )
+  }, [user, authReady]);
 
   return (
     <div className={styles.guides}>
-      <h2>All Guides</h2>
-    </div> 
-  )
+      {!authReady && <div>Loading...</div>}
+
+      {error && (
+        <div className={styles.error}>
+          <p>{error}</p>
+        </div>
+      )}
+
+      {guides &&
+        guides.map((guide) => (
+          <div key={guide.title} className={styles.card}>
+            <h3>{guide.title}</h3>
+            <h4>Written by {guide.author}</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti
+              itaque reprehenderit non iste debitis dicta? Possimus animi
+              obcaecati nihil optio ipsam accusantium dolore nisi recusandae
+              placeat ut? Facere, neque in?
+            </p>
+          </div>
+        ))}
+    </div>
+  );
 }
